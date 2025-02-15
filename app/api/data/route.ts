4e11e175faad63221f53706329675d01b23f9e21
Ian Langworth <ian@northpole.security>
2025-02-14 18:59:01 -0800

    Enhance /api/data route with authentication state logging

--- app/api/data/route.ts
+++ app/api/data/route.ts
@@ -2,13 +2,12 @@ import { withAuth } from "@workos-inc/authkit-nextjs";
 
 export async function GET() {
 	const { user } = await withAuth();
-
-	if (!user) {
-		return new Response('Unauthorized', { status: 401 });
-	}
+	const suffix = user ? `(as ${user.firstName})` : '(as anonymous)';
 
 	console.log('Waiting...');
 	await new Promise((resolve) => setTimeout(resolve, 300));
 	console.log('Done waiting');
-	return Response.json(Array.from({ length: 10 }, (_, i) => `Item ${i + 1}`));
+
+	// Just some dummy data with a suffix to indicate the authentication state
+	return Response.json(Array.from({ length: 10 }, (_, i) => `Item ${i + 1} ${suffix}`));
 }
